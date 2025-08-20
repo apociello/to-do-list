88a5a9d9da9e99ba1881c6bd38ad5f8737e26d59
pociello <asierpociello@gmail.com>
2025-08-20 11:26:02 +0200

    Add deleteNote() static func to the Note class

--- src/notes/note.js
+++ src/notes/note.js
@@ -1,8 +1,14 @@
 export class Note {
     static noteList = [];
+
     constructor(title) {
         this.title = title;
         this.text = '';
         Note.noteList.push(this)
+    }
+
+    static deleteNote(note) {
+        const index = Note.noteList.indexOf(note);
+        Note.noteList.splice(index, 1)
     }
 }
