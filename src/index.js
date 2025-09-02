b7b4797b5af2fc6ca01d90bb49ba43ac81366e72
pociello <asierpociello@gmail.com>
2025-09-02 10:50:48 +0200

    create basic html dialog for adding projects: - For now just appear in the screen but without functionlity and style

--- src/index.js
+++ src/index.js
@@ -44,6 +44,17 @@ thisWeekBtn.addEventListener('click', () => {
   thisWeekPage(allProjects)
 })
 
+//ADD PROJECT DIALOG
+const dialog = document.getElementById('project-dialog');
+
+const closeDialog = document.querySelector('.close');
+closeDialog.addEventListener('click', () => dialog.close());
+
+const openDialog = document.getElementById('open-dialog');
+openDialog.addEventListener('click', () => dialog.showModal());
+
+
+
 
 
 
---
+++ src/pages/project/projectDialog.js
@@ -0,0 +1,9 @@
+
+/*const dialog = document.getElementById('project-dialog');
+
+const closeDialog = document.querySelector('.close');
+closeDialog.addEventListener('click', () => dialog.close());
+
+const openDialog = document.getElementById('open-dialog');
+openDialog.addEventListener('click', () => dialog.showModal());
+*/
